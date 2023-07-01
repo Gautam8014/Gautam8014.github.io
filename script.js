13a28b7564412cc5d032699a49550d48520a681d
Gautam8014 <flt.gautam@gmail.com>
2023-07-01 05:37:49 +0530

    done

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let menu=document.getElementById("menu-icon");
+let navbar=document.querySelector(".navbar");
+let m=document.querySelector(".main-body")
+let count=0;
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+    
+    m.style.marginTop="200px";
+    count++;
+    if(count%2==0){
+        m.style.marginTop="0px";
+
+    }
+
+    if(m.classList.contains("m-t")){
+        m.classList.remove("m-t");
+    }
+    else{
+        m.classList.add("m-t");
+    }
+});
+
+
+GitHubCalendar(".calendar", "Gautam8014", { responsive: true });
+GitHubCalendar(".calendar", "Gautam8014", { responsive: true });
+
+
+function resume(){
+    window.open("https://drive.google.com/file/d/1j3dNEXtqhBOwVfU5nlGcpsv7eVTUzEzi/view?usp=sharing")
+}
