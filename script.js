4ca272414d61d65131150147c445450961acca78
Gautam8014 <flt.gautam@gmail.com>
2023-10-05 03:40:21 +0530

    nav

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+let menu = document.getElementById("menu-icon");
+let navbar = document.querySelector(".navbar");
+let m = document.querySelector(".main-body");
+let count = 0;
 
-let menu=document.getElementById("menu-icon");
-let navbar=document.querySelector(".navbar");
-let m=document.querySelector(".main-body")
-let count=0;
-menu.addEventListener('click',()=>{
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('bx-x'); // Toggle hamburger and close icon
+    navbar.classList.toggle('open'); // Toggle the visibility of the navbar
     
-    m.style.marginTop="200px";
-    count++;
-    if(count%2==0){
-        m.style.marginTop="0px";
-
-    }
-
-    if(m.classList.contains("m-t")){
+    // You can control the main body's margin-top if needed.
+    // For example, set a class on the main body and toggle it.
+    if (count % 2 === 0) {
         m.classList.remove("m-t");
-    }
-    else{
+    } else {
         m.classList.add("m-t");
     }
+    
+    count++;
 });
+
+// You may also want to close the menu when a menu item is clicked
+document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('bx-x');
+        navbar.classList.remove('open');
+        m.classList.remove('m-t');
+        count = 0;
+    });
+});
+
 
 
 GitHubCalendar(".calendar", "Gautam8014", { responsive: true });
@@ -31,3 +38,4 @@ GitHubCalendar(".calendar", "Gautam8014", { responsive: true });
 function resume(){
     window.open("https://drive.google.com/file/d/1j3dNEXtqhBOwVfU5nlGcpsv7eVTUzEzi/view?usp=sharing")
 }
+
